6fa42a7559defa99986fc01f3748674b9e48f927
angelblue05 <angelblue05@users.noreply.github.com>
2014-12-01 03:47:44 -0600

    Update for-loop.js

--- codeacademy/javascript/for-loop.js
+++ codeacademy/javascript/for-loop.js
@@ -9,3 +9,9 @@ for (var counter = 1; counter < 11; counter++) {
 for (var i = 4; i < 24; i = i + 1) {
 	console.log(i);
 }
+
+// another challenge
+
+for (var i = 100; i > 0; i -= 5) {
+    console.log(i)   
+}
