430be5ca3bc857ea036e0988ef4dd06c1900d52b
angelblue05 <angelblue05@users.noreply.github.com>
2014-12-02 00:08:09 -0600

    Update loop-if-array.js

--- codeacademy/javascript/loop-if-array.js
+++ codeacademy/javascript/loop-if-array.js
@@ -1,3 +1,5 @@
+/*jshint multistr:true */
+
 var text = "Hey, how are you \
 doing? My name is Tamara. I'm sorry \
 you haven't head what I said. My name \
@@ -9,5 +11,17 @@ var hits = []
 for (var i = 0; i < text.length; i++) {
     if (myName[0] === text[i]) {
         
+        var characterSave = "";
+        for (var j = i; j < myName.length + i; j++) {
+            characterSave = text[j];
+            hits.push(characterSave);  
+        }
     }
+}
+
+if (hits.length === 0) {
+    console.log("Your name wasn't found!");   
+}
+else    {
+    console.log(hits);   
 }
