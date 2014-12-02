d82d42be4ba33afb91332e34fcfed241e885a357
angelblue05 <angelblue05@users.noreply.github.com>
2014-12-01 19:58:50 -0600

    Create loop-if-array.js

---
+++ codeacademy/javascript/loop-if-array.js
@@ -0,0 +1,13 @@
+var text = "Hey, how are you \
+doing? My name is Tamara. I'm sorry \
+you haven't head what I said. My name \
+is Tamara.";
+
+var myName = "Tamara"
+var hits = []
+
+for (i = 0; i < text.length; i++) {
+    if (myName[0] === text[0]) {
+        
+    }
+}
