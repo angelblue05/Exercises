1bc74b90e29cdfc2ac8f8680dd9b8a8321ca5b62
angelblue05 <angelblue05@users.noreply.github.com>
2014-12-03 17:36:47 -0600

    Create objects.js

---
+++ codeacademy/javascript/objects.js
@@ -0,0 +1,12 @@
+var friends = {
+bill: {
+    firstName: 'Bill',
+    lastName: 'Buffalo',
+    number: "(433) 555-5555"
+    },
+steve: {
+    firstName: 'Steve',
+    lastName: 'Smith',
+    number: "(406) 555-5555"
+    },
+};
