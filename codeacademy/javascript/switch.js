09c5e6f1a70d0dd60187749aa40b5092517cabf2
angelblue05 <angelblue05@users.noreply.github.com>
2014-12-02 22:47:32 -0600

    Create switch.js

---
+++ codeacademy/javascript/switch.js
@@ -0,0 +1,17 @@
+// Write your code below!
+
+var switchAnswer = prompt("What is your favorite sport?");
+
+switch (switchAnswer)   {
+    case 'hockey':
+        console.log("Hockey is boring to watch.");
+    break;
+    case 'soccer':
+        console.log("I'd only watch soccer if there was nothing else on.");
+    break;
+    case 'ufc':
+        console.log("Now you're talking!");
+    break;
+    default:
+        console.log("Do you even know what sports there is?");
+}
