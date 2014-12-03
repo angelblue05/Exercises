2ae868411c581339ca7b0acd1345f404b29c2627
angelblue05 <angelblue05@users.noreply.github.com>
2014-12-02 21:28:47 -0600

    Create dragon-game.js

---
+++ codeacademy/javascript/dragon-game.js
@@ -0,0 +1,26 @@
+var slaying = true;
+var youHit = Math.floor(Math.random() * 2)
+var damageThisRound = Math.floor(Math.random()*5 + 1)
+var totalDamage = 0
+
+
+while (slaying) {
+    if (youHit)   {
+        console.log("You hit the dragon!");
+        totalDamage += damageThisRound;
+        console.log("You've dealt " + totalDamage + " to the dragon.");
+        
+        if (totalDamage >= 4)   {
+            console.log("You slew the dragon!");
+            var slaying = false;
+        }
+        else    {
+            youHit = Math.floor(Math.random() * 2);   
+        }
+    }
+    else    {
+        console.log("The dragon defeated you.");
+    }
+        
+    var slaying = false;
+}
