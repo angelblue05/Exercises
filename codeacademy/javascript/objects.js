507a50b4be35126688a869ca187d05a037eeda16
angelblue05 <angelblue05@users.noreply.github.com>
2014-12-03 18:50:23 -0600

    Update objects.js

--- codeacademy/javascript/objects.js
+++ codeacademy/javascript/objects.js
@@ -2,11 +2,30 @@ var friends = {
 bill: {
     firstName: 'Bill',
     lastName: 'Buffalo',
-    number: "(433) 555-5555"
+    number: "(433) 555-5555",
+    address: ['One Microsoft Way', 'Redmond', 'WA', '98052']
     },
 steve: {
     firstName: 'Steve',
     lastName: 'Smith',
-    number: "(406) 555-5555"
+    number: "(406) 555-5555",
+    address: ['Two Google circle', 'San Francisco', 'FL', '96056']
     },
 };
+
+var list = function () {
+    for  (var name in friends) {
+            console.log(name);
+    }
+};
+
+var search = function (name) {
+    for (var lookUp in friends) {
+        if(friends[lookUp].firstName === name) {
+            console.log(friends[lookUp]);
+            return friends[lookUp];
+        }
+    }
+};
+
+search("Bill");
