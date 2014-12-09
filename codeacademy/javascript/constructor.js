50c1ed37de1e9c47fd5cf782b7c1e1ae6dc99ea1
angelblue05 <angelblue05@users.noreply.github.com>
2014-12-09 03:12:27 -0600

    Update constructor.js

--- codeacademy/javascript/constructor.js
+++ codeacademy/javascript/constructor.js
@@ -8,3 +8,21 @@ var bob = new Person("Bob Smith", 30);
 var susan = new Person("Susan Jordan", 25);
 // help us make george, whose name is "George Washington" and age is 275
 var george = new Person("George Washington", 275);
+
+
+// another example
+function Rabbit(adjective) {
+    this.adjective = adjective;
+    this.describeMyself = function() {
+        console.log("I am a " + this.adjective + " rabbit");
+    };
+}
+
+// now we can easily make all of our rabbits
+var rabbit1 = new Rabbit("fluffy");
+var rabbit2 = new Rabbit("happy");
+var rabbit3 = new Rabbit("sleepy");
+
+rabbit1.describeMyself();
+rabbit2.describeMyself();
+rabbit3.describeMyself();
