97cfe6460eea59767b4ff3e10e6eae52893af87f
angelblue05 <angelblue05@users.noreply.github.com>
2014-12-11 01:07:04 -0600

    Update method.js

--- codeacademy/javascript/method.js
+++ codeacademy/javascript/method.js
@@ -30,3 +30,16 @@ square.calcArea = function () {
 
 var p = square.calcPerimeter();
 var a = square.calcArea();
+
+// refresher on methods
+function Circle (radius) {
+    this.radius = radius;
+    this.area = function () {
+        return Math.PI * this.radius * this.radius;
+        
+    };
+    // define a perimeter method here
+    this.perimeter = function () {
+        return this.radius * Math.PI * 2
+    };
+};
