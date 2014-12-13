45ca2fb5701dcbdcf73cac2a3fdd0dee0f82147a
angelblue05 <angelblue05@users.noreply.github.com>
2014-12-13 02:45:39 -0600

    Update inheritance.js

--- codeacademy/javascript/inheritance.js
+++ codeacademy/javascript/inheritance.js
@@ -18,3 +18,20 @@ Penguin.prototype = new Animal();
 
 var penguin = new Penguin("Happy Feet");
 penguin.sayName();
+
+// another example
+
+function Penguin(name) {
+    this.name = name;
+    this.numLegs = 2;
+}
+
+// create your Emperor class here and make it inherit from Penguin
+function Emperor(name) {
+    this.name = name;  
+};
+
+Emperor.prototype = new Penguin();
+// create an "emperor" object and print the number of legs it has
+var emperor = new Emperor("Crown Leader");
+console.log(emperor.numLegs);
