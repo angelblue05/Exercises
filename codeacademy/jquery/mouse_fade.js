142f44f2fbd79864aa059549763f31e8e1f63e4f
angelblue05 <angelblue05@users.noreply.github.com>
2014-12-21 03:19:55 -0600

    Update mouse_fade.js

--- codeacademy/jquery/mouse_fade.js
+++ codeacademy/jquery/mouse_fade.js
@@ -3,3 +3,15 @@ $(document).ready(function() {
         $('div').fadeTo('fast', 1);  
     });
 });
+
+// another example
+
+$(document).ready(function() {
+    $('div').mouseenter(function() {
+        $('div').fadeTo('fast', 1);  
+    });
+    
+    $('div').mouseleave(function() {
+        $('div').fadeTo('fast', 0.5);
+    });
+});
