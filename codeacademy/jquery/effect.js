e8f473d9b334d82754f7bb926cba98db0d94506a
angelblue05 <angelblue05@users.noreply.github.com>
2014-12-22 17:50:27 -0600

    Update effect.js

--- codeacademy/jquery/effect.js
+++ codeacademy/jquery/effect.js
@@ -3,3 +3,11 @@ $(document).ready(function() {
         $(this).effect('explode');    
     }); 
 });
+
+// another example
+
+$(document).ready(function() {
+    $('div').click(function() {
+        $(this).effect('bounce', {times:3}, 500);    
+    }); 
+});
