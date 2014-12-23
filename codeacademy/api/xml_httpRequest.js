3201f76b4dbc61bfefb2bc67a14a937e23dca91e
angelblue05 <angelblue05@users.noreply.github.com>
2014-12-22 23:10:27 -0600

    Update xml_httpRequest.js

--- codeacademy/api/xml_httpRequest.js
+++ codeacademy/api/xml_httpRequest.js
@@ -4,3 +4,15 @@ xhr.open("GET", "http://www.codecademy.com/", false);
 xhr.send();
 console.log(xhr.status);
 console.log(xhr.statusText);
+
+// another example
+
+// Add your code below this line!
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "http://www.codecademy.com/", false);
+xhr.send();
+
+// Add your code above this line!
+
+console.log(xhr.status);
+console.log(xhr.statusText);
