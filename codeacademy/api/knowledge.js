f14947870719e1ad3c75651708f9edddbfa44b2a
angelblue05 <angelblue05@users.noreply.github.com>
2014-12-22 21:35:20 -0600

    Create knowledge.js

---
+++ codeacademy/api/knowledge.js
@@ -0,0 +1,6 @@
+The number of HTTP methods you'll use is quite small—there are just four HTTP "verbs" you'll need to know! They are:
+
+GET: retrieves information from the specified source (you just saw this one!)
+POST: sends new information to the specified source.
+PUT: updates existing information of the specified source.
+DELETE: removes existing information from the specified source.
