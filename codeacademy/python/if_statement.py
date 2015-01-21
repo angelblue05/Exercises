5dd60ecc40684a326de5704e1ee27a511604fdb4
angelblue05 <angelblue05@users.noreply.github.com>
2015-01-21 03:56:54 -0600

    Update if_statement.py

--- codeacademy/python/if_statement.py
+++ codeacademy/python/if_statement.py
@@ -8,3 +8,17 @@ def using_control_again():
 
 print using_control_once()
 print using_control_again()
+
+# another example
+
+def greater_less_equal_5(answer):
+    if answer > 5:
+        return 1
+    elif answer < 5:          
+        return -1
+    else:
+        return 0
+        
+print greater_less_equal_5(4)
+print greater_less_equal_5(5)
+print greater_less_equal_5(6)
