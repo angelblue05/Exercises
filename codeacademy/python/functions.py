a438ccf2e27b8f4e20fdb80aed8caad7cd8403ba
angelblue05 <angelblue05@users.noreply.github.com>
2015-01-21 20:47:21 -0600

    Create functions.py

---
+++ codeacademy/python/functions.py
@@ -0,0 +1,16 @@
+def one_good_turn(n):
+    return n + 1
+    
+def deserves_another(n):
+    return one_good_turn(n) + 2
+    
+# another function
+
+def cube(number):
+    return number**3
+
+def by_three(number):
+    if number % 3 == 0:
+        return cube(number)
+    else:
+        return False
