61ce933cd2f43dfb82bf0d56f3940701daad08ea
angelblue05 <angelblue05@users.noreply.github.com>
2015-01-22 02:31:32 -0600

    Update list.py

--- codeacademy/python/list.py
+++ codeacademy/python/list.py
@@ -6,9 +6,17 @@ suitcase.append("bathing suit")
 suitcase.append("sun block")
 suitcase.append("book")
 
-
-
 list_length = len(suitcase) # Set this to the length of suitcase
 
 print "There are %d items in the suitcase." % (list_length)
 print suitcase
+
+# another example
+
+animals = ["aardvark", "badger", "duck", "emu", "fennec fox"]
+duck_index =  animals.index("duck")  # Use index() to find "duck"
+
+# Your code here!
+animals.insert(duck_index, "cobra") 
+
+print animals # Observe what prints after the insert operation
