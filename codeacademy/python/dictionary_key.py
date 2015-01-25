0c3118d5486018459a20e6e77a931184a01fa816
angelblue05 <angelblue05@users.noreply.github.com>
2015-01-25 03:10:40 -0600

    Update dictionary_key.py

--- codeacademy/python/dictionary_key.py
+++ codeacademy/python/dictionary_key.py
@@ -15,3 +15,30 @@ dict_name[key] = new_value
 
 # delete a key from a dictionary
 del dict_name[key_name]
+
+# another example
+my_dict = {
+    "fish": ["c", "a", "r", "p"],
+    "cash": -4483,
+    "luck": "good"
+}
+print my_dict["fish"][0]
+
+# another example
+inventory = {
+    'gold' : 500,
+    'pouch' : ['flint', 'twine', 'gemstone'], # Assigned a new list to 'pouch' key
+    'backpack' : ['xylophone','dagger', 'bedroll','bread loaf']
+}
+
+# Adding a key 'burlap bag' and assigning a list to it
+inventory['burlap bag'] = ['apple', 'small ruby', 'three-toed sloth']
+
+# Sorting the list found under the key 'pouch'
+inventory['pouch'].sort() 
+
+# Your code here
+inventory['pocket'] = ['seashell', 'strange berry', 'lint']
+inventory['backpack'].sort()
+inventory['backpack'].remove('dagger')
+inventory['gold'] += 50
