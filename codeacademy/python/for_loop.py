ed024d9d065bc5ce15e5879b4a4281a6451d1fb5
angelblue05 <angelblue05@users.noreply.github.com>
2015-01-25 03:38:03 -0600

    Update for_loop.py

--- codeacademy/python/for_loop.py
+++ codeacademy/python/for_loop.py
@@ -25,3 +25,35 @@ webster = {
 # Print the key values
 for key in webster:
     print webster[key]
+
+# another example
+a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+for number in a:
+    if number % 2 == 0:
+        print number
+        
+ # another example
+def fizz_count(x):
+    count = 0
+    
+    for item in x:
+        if item == "fizz":
+            count += 1
+    
+    return count
+
+# another example - loop thru strings
+for letter in "Codecademy":
+    print letter
+    
+# Empty lines to make the output pretty
+print
+print
+
+word = "Programming is fun!"
+
+for letter in word:
+    # Only print out the letter i
+    if letter == "i":
+        print letter
