2c9d55210decb657c16276310b9386fa4a414cde
angelblue05 <angelblue05@users.noreply.github.com>
2015-01-25 03:55:09 -0600

    Update supermarket.py

--- codeacademy/python/supermarket.py
+++ codeacademy/python/supermarket.py
@@ -1,3 +1,5 @@
+# manager of the supermarket
+
 prices = {
     "banana": 4,
     "apple": 2,
@@ -25,3 +27,32 @@ for key in prices:
     total += worth
 
 print total
+
+# shopper at the supermarket
+
+shopping_list = ["banana", "orange", "apple"]
+
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+    
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+
+# Write your code below!
+def compute_bill(food):
+    total = 0
+    
+    for item in food:
+        if stock[item] > 0:
+            total += prices[item]
+            stock[item] -= 1
+    
+    return total
