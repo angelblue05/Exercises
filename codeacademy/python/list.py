3d1f6fd177dc79e4e652ef5c8f75ed8e28890e3b
angelblue05 <angelblue05@users.noreply.github.com>
2015-01-26 22:48:18 -0600

    Update list.py

--- codeacademy/python/list.py
+++ codeacademy/python/list.py
@@ -23,3 +23,17 @@ print animals # Observe what prints after the insert operation
 
 # remove an item from a list
 list.remove(item)
+
+# another example - list of lists
+n = [[1, 2, 3], [4, 5, 6, 7, 8, 9]]
+# Add your function here
+def flatten(lists):
+    results = []
+    
+    for numbers in lists:
+        for num in numbers:
+            results.append(num)
+    
+    return results
+
+print flatten(n)
